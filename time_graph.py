ced6842dba9b9a79ca45a1eb74a6cee87f3b775a
ruansongsong <ruansongsong@hotmail.com>
2015-12-11 21:54:26 +0800

    write readme

--- time_graph.py
+++ time_graph.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-#绘制转发时间图
+#绘制转发时间扩散、增长图
 import matplotlib.pyplot as plt
 import networkx as nx 
 from datetime import datetime
@@ -10,10 +10,8 @@ with open('data.csv', 'r') as f:
 	    time= line.strip().split(',')[-1]
 	    time = time[1:-1]
 	    rt_time.append(time)
-#day = [('2014-08-0'+str(i)[:1]+'-'+str(i)[1:3]) for i in rt_time]
 day = [(str(i)[0:10]+'-'+str(i)[11:13]) for i in rt_time]
 day = [datetime.strptime(d, '%Y-%m-%d-%H') for d in day]
-#time
 day_weibo = datetime.strptime('2015-12-07-10', '%Y-%m-%d-%H') #源微博发出时间
 hours = [(i-day_weibo).total_seconds()/3600 for i in day]
 values, base = np.histogram(hours, bins = 40)
